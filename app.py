251aa37f0f8055361be2216345edf045dcf5ddd9
Bealby <mgbealby@gmail.com>
2020-06-07 12:46:58 +0000

    Removed templates

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from flask import Flask
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://bealby:<password>@myfirstcluster-6x5n6.mongodb.net/<dbname>?retryWrites=true&w=majority'
-
-mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+def hello():
+    return 'Hello World ...again'
 
 
 if __name__ == '__main__':
